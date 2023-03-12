9716d9e124cc57e74a27173ee4494c5217033d86
Gabriel Machado Jr <73040950+gabrieltmjr@users.noreply.github.com>
2023-03-12 17:15:08 +0000

    Objects for real

---
+++ src/board.py
@@ -0,0 +1,5 @@
+class Board:
+    num_rows = 5
+    num_cols = 5
+    def __init__(self, tiles):
+        self.tiles = tiles
---
+++ src/tile.py
@@ -0,0 +1,5 @@
+class Tile:
+    def __init__(self, position, color, center_tile):
+        self.position = position
+        self.color = color
+        self.center_tile = center_tile
