4ff79b2328c27493e6aec862b21eb3fb43399d37
gabrieltmjr <gabrieltmjr@gmail.com>
2023-03-13 20:27:45 +0000

    models base defined

--- src/board.py
+++
@@ -1,5 +0,0 @@
-class Board:
-    num_rows = 5
-    num_cols = 5
-    def __init__(self, tiles):
-        self.tiles = tiles
---
+++ src/model/position.py
@@ -0,0 +1,8 @@
+class Position:
+    """This class represents an object position in 2D using x and y components.
+       var: x - X axis object position.
+       var: y - Y axis object position.
+    """
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
