8bea70403b73775a894bd1bc6754dd05dee89024
Miguel Curval <up201105191@edu.fe.up.pt>
2023-03-20 18:42:41 +0000

    Stub

--- src/piece.py
+++
@@ -1,8 +0,0 @@
-class Piece:
-    def __init__(self, numbered, king, position, color, number_value, my_tiles):
-        self.numbered = numbered
-        self.king = king
-        self.position = position
-        self.color = color
-        self.number_value = number_value
-        self.my_tiles = my_tiles
