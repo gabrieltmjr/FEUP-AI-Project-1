438535e056430bab0c9538a8311bbd3c095af973
gabrieltmjr <gabrieltmjr@gmail.com>
2023-03-27 12:25:34 +0100

    Images

--- src/model/tile.py
+++ src/model/tile.py
@@ -3,7 +3,7 @@ import src.model.position as Position
 class Tile:
     """This class represents a board Tile.
        var: position - Position of the tile in the board.
-       var: color - color of the tile. 0 - neutral, 1 - white, 2 - blue
+       var: color - color of the tile. -1: neutral, 0: white, 1:blue
     """
     def __init__(self, position: Position, color: int):
         self.position = position
