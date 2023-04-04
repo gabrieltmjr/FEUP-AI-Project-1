595ad860cd17fbf346c0c7ff99dee9d11b2306d6
Antonio Augusto Brito de Sousa <up202000705@fe.up.pt>
2023-04-04 17:27:19 +0100

    Board implemented

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from CIFRA_Code25 import CIFRACode25State, print_state
 from MonteCarloTreeSearch import MCTS
-
+from view.View import Screen
 if __name__=="__main__":
     #state = CIFRACode25State('Sum')
     #state = CIFRACode25State('Dash')
@@ -8,15 +8,18 @@ if __name__=="__main__":
     mcts = MCTS('seconds', 0.1)
     #mcts = MCTS('milliseconds', 1000)
     #mcts = MCTS('simulations', 500)
-
+    screen = Screen()
+   
     while not state.is_terminal():
         print_state(state)
         print()
+        
         move = mcts.search(initial_state=state)
         print('Best move:')
         print(move)
         print()
         state = state.make_move(move) # e aqui está o proximo state
+        screen.show_board(state.board)
     print_state(state)
     print(state.pieces)
     print(state.get_winner())
--- src/view/View.py
+++ src/view/View.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 27 11:25:35 2023
-
-@author: Antonio Augusto
-"""
-
 import pygame
-
+import random
+import numpy as np
 class Screen:
     def __init__(self):
         # screen dimensions
-        screen_width = 500
-        screen_height = 500
+        
+        self.board_color = np.random.randint(0, 2, size=(5, 5))
+        
+        self.screen_width = 500
+        self.screen_height = 500
+        self.blueTile = pygame.image.load('./img/0.png')
+        self.whiteTile = pygame.image.load('img/1.png')
+     
         # flag to know if game menu has been showed
         self.menu_showed = False
         # flag to set game loop
@@ -25,7 +25,7 @@ class Screen:
         pygame.font.init()
 
         # create game window
-        self.screen = pygame.display.set_mode([screen_width, screen_height])
+        self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
 
         # title of window
         window_title = "Cifra"
@@ -36,5 +36,11 @@ class Screen:
         pygame.display.flip()
         
         
+    def show_board(self,board):
         
-    
+        
+        for line in range(0,len(board)):
+            row = line%4
+            print(row)
+# paint screen one time
+        pygame.display.flip()        
